a832c67a439e3ffee5065de8758c91be34e0f0d5
Christopher Crow <0chriscrew0@gmail.com>
2025-11-05 23:48:18 -0600

    change reset to post

--- dist/index.js
+++ dist/index.js
@@ -7,9 +7,9 @@ const app = express();
 const PORT = 8080;
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.use(middlewareLogResponses);
-app.get("/healthz", handlerReadiness);
-app.get("/metrics", handlerMetrics);
-app.get("/reset", handlerReset);
+app.get("/api/healthz", handlerReadiness);
+app.get("/admin/metrics", handlerMetrics);
+app.post("/admin/reset", handlerReset);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
