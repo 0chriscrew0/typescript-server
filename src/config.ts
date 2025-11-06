4723a1e0df54940777e5c1dc3e317a8b5cfb9b2c
Christopher Crow <0chriscrew0@gmail.com>
2025-11-06 12:12:08 -0600

    drizzle setup

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,15 @@
+process.loadEnvFile();
+
+if (!process.env.DB_URL) {
+  throw new Error("set dburl env variable");
+}
+
 type APIConfig = {
   fileserverHits: number;
+  dbURL: string;
 };
 
 export const config: APIConfig = {
   fileserverHits: 0,
+  dbURL: process.env.DB_URL,
 };
