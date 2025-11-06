4d681fce89c413607893509fc50726b19413ceab
Christopher Crow <0chriscrew0@gmail.com>
2025-11-06 01:22:12 -0600

    add profanity filter to validate_chirps handler

--- src/api/validate_chirp.ts
+++ src/api/validate_chirp.ts
@@ -1,13 +1,22 @@
 import type { Request, Response } from "express";
 
 export async function handlerValidateChirp(req: Request, res: Response) {
+  const profane = ["kerfuffle", "sharbert", "fornax"];
   try {
     const parsedBody = req.body;
     if (parsedBody.body.length > 140) {
       res.status(400).send({ error: "Chirp is too long" });
       return;
     }
-    res.status(200).send({ valid: true });
+    let words = parsedBody.body.split(" ");
+    for (let i in words) {
+      if (profane.includes(words[i].toLowerCase())) {
+        words[i] = "****";
+      }
+    }
+    const cleanBody = words.join(" ");
+    console.log(cleanBody);
+    res.status(200).send({ cleanedBody: cleanBody });
   } catch (err) {
     res.status(400).send({ error: "Something went wrong" });
   }
