5dcb4c26d8603c6ac6f07abbea098ac2694a482d
Christopher Crow <0chriscrew0@gmail.com>
2025-11-05 18:01:12 -0600

    add logging middleware

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { handlerReadiness } from "./api/readiness.js";
+import { middlewareLogResponses } from "./middleware.js";
 const app = express();
 const PORT = 8080;
 app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
 app.get("/healthz", handlerReadiness);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
