847114fc2fdd6485b4efaee90764a022335b5e86
Christopher Crow <0chriscrew0@gmail.com>
2025-11-06 18:15:01 -0600

    add delete chirp route and handler

--- src/db/queries/chirps.ts
+++ src/db/queries/chirps.ts
@@ -1,43 +1,22 @@
-import { BadRequestError } from "../../api/errors.js";
-import { db } from "../index.js";
-import { NewChirp, chirps } from "../schema.js";
 import { eq } from "drizzle-orm";
-
-function validateAndCleanChirp(chirp: NewChirp) {
-  const profane = ["kerfuffle", "sharbert", "fornax"];
-
-  if (chirp.body.length > 140) {
-    throw new BadRequestError("Chirp is too long. Max length is 140");
-  }
-  let words = chirp.body.split(" ");
-  for (let i in words) {
-    if (profane.includes(words[i].toLowerCase())) {
-      words[i] = "****";
-    }
-  }
-  const cleanBody = words.join(" ");
-  return cleanBody;
-}
+import { db } from "../index.js";
+import { chirps, NewChirp } from "../schema.js";
 
 export async function createChirp(chirp: NewChirp) {
-  const newChirp = {
-    body: validateAndCleanChirp(chirp),
-    userId: chirp.userId,
-  };
-  const [result] = await db
-    .insert(chirps)
-    .values(newChirp)
-    .onConflictDoNothing()
-    .returning();
-  return result;
+  const [rows] = await db.insert(chirps).values(chirp).returning();
+  return rows;
 }
 
 export async function getChirps() {
-  const results = await db.select().from(chirps);
-  return results;
+  return db.select().from(chirps);
 }
 
 export async function getChirp(id: string) {
   const [result] = await db.select().from(chirps).where(eq(chirps.id, id));
   return result;
+}
+
+export async function deleteChirp(id: string) {
+  const rows = await db.delete(chirps).where(eq(chirps.id, id)).returning();
+  return rows.length > 0;
 }
